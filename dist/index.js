e2ff5dec310febdb0bff2011e9fc66c005963c2b
maclerylin(林剑) <maclerylin@tencent.com>
2024-07-17 10:32:53 +0800

    feat: 修改默认导出

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const vue3_1 = require("./vue3");
-const path_1 = __importDefault(require("path"));
-const result = (0, vue3_1.analysisVue)(path_1.default.resolve(process.cwd(), '__test__/demo-comp3/index.vue'));
-console.log(JSON.stringify(result));
-// export default {
-//   analysisVue,
-// };
+// const result = analysisVue(path.resolve(process.cwd(), '__test__/demo-comp/index.vue'));
+// console.log(JSON.stringify(result));
+exports.default = {
+    analysisVue: vue3_1.analysisVue,
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { analysisVue } from "./vue3";
 import path from 'path';
 
 
-const result = analysisVue(path.resolve(process.cwd(), '__test__/demo-comp3/index.vue'));
-console.log(JSON.stringify(result));
-// export default {
-//   analysisVue,
-// };
+// const result = analysisVue(path.resolve(process.cwd(), '__test__/demo-comp/index.vue'));
+// console.log(JSON.stringify(result));
+export default {
+  analysisVue,
+};
